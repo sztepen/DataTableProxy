b7229e8892966fdccafd3f7e0efbe68bf5cce1ec
sztepen <sztepen@gmail.com>
2021-01-29 18:43:09 +0100

    nuget and nunit fixes. tests work

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -11,12 +11,7 @@ namespace Tests
 		[STAThread]
 		static void Main(string[] args)
 		{
-			string[] my_args = { Assembly.GetExecutingAssembly().Location };
-
-			int returnCode = NUnit.ConsoleRunner.Runner.Main(my_args);
-
-			if (returnCode != 0)
-				Console.Beep();
+			
 		}
 	}
 }
